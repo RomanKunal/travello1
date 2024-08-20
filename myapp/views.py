3da8dee804519f993ebcb566304ee919f10f145e
RomanKunal <0221csds107@niet.co.in>
2024-08-20 22:46:44 +0530

    refactor: Update destination data and rendering logic in index view

--- myapp/views.py
+++ myapp/views.py
@@ -6,19 +6,20 @@ def index(request):
     dest1.desc="The city that can't sleep"
     dest1.img="destination_10.jfif"
     dest1.price="From Rs 70"
-    return render(request, 'index.html',{'dest1':dest1})
 
-""""
     dest2=Destination()
     dest2.name="Delhi"
     dest2.desc="The city of forts"
-    dest2.img="destination_7.jpeg"
+    dest2.img="destination_7.jpg"
     dest2.price="From Rs 80"
     
     dest3=Destination()
     dest3.name="Bangalore"
     dest3.desc="The city of gardens"
-    dest3.img="destination_8.jpeg"
+    dest3.img="destination_8.jpg"
     dest3.price="From Rs 90"
-    dests=[dest1,dest2,dest3]
-"""
+    data=[dest1,dest2,dest3]
+    return render(request, 'index.html',{'dests':data})
+
+
+    
